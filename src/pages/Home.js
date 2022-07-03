94dea3fd3c100c824ca33a98dc22d378e427f70a
Wallace Júnior <wflj1997@hotmail.com>
2022-07-03 17:16:59 -0300

    Utilizando StyleSheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,61 @@
-import { Text, View } from "react-native";
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "white",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: "#a370f7",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+});
 
 export const Home = () => (
-  <View
-    style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <Text>React Native</Text>
+  <SafeAreaView style={styles.container}>
+    <Text style={styles.title}>Welcome, Wallace</Text>
 
-    <Text>Ignite</Text>
-  </View>
+    <TextInput
+      style={styles.input}
+      placeholder="New Skill"
+      placeholderTextColor="#555"
+    />
+
+    <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+      <Text style={styles.buttonText}>Add</Text>
+    </TouchableOpacity>
+
+    <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
+  </SafeAreaView>
 );
