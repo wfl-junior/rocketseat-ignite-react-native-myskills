28693a370515c0652b54b7f110d24fe8fb33275a
Wallace Júnior <wflj1997@hotmail.com>
2022-07-03 17:00:37 -0300

    Estrutura JSX

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export const Home = () => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text>React Native</Text>
+
+    <Text>Ignite</Text>
+  </View>
+);
