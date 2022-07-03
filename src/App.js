6e55c5784c7b9a69a3825e03bbae7ba96cddb0ff
Wallace Júnior <wflj1997@hotmail.com>
2022-07-03 18:39:12 -0300

    Finalizando essa etapa do App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { Fragment } from "react";
+import { StatusBar } from "react-native";
 import { Home } from "./pages/Home";
 
-export const App = () => <Home />;
+export const App = () => (
+  <Fragment>
+    <StatusBar barStyle="light-content" backgroundColor="#121015" />
+    <Home />
+  </Fragment>
+);
