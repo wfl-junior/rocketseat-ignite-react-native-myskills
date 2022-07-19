9f1ec681d688a395fd743b347fcb3df096b47adf
Wallace Júnior <wflj1997@hotmail.com>
2022-07-19 14:27:32 -0300

    Carregamento da Splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { StatusBar } from "react-native";
+import SplashScreen from "react-native-splash-screen";
 import { Home } from "./screens/Home";
 
-export const App = () => (
-  <Fragment>
-    <StatusBar barStyle="light-content" backgroundColor="#121015" />
-    <Home />
-  </Fragment>
-);
+export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <Fragment>
+      <StatusBar barStyle="light-content" backgroundColor="#121015" />
+      <Home />
+    </Fragment>
+  );
+};
