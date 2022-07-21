a237d663ff0f95021b6867fefb8c7861839466e4
Wallace Júnior <wflj1997@hotmail.com>
2022-07-20 23:45:15 -0300

    Rastreando erros em produção

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import * as Sentry from "@sentry/react-native";
 import { Fragment, useEffect } from "react";
 import { StatusBar } from "react-native";
 import codePush from "react-native-code-push";
 import SplashScreen from "react-native-splash-screen";
 import { Home } from "./screens/Home";
+
+Sentry.init({
+  dsn: "https://a40fe8d16faf41adb97321c3a7e9f202@o1327587.ingest.sentry.io/6588944",
+  tracesSampleRate: 1.0,
+  enableNative: false,
+});
 
 const AppComponent: React.FC = () => {
   useEffect(() => {
@@ -21,6 +28,8 @@ const AppComponent: React.FC = () => {
   );
 };
 
-export const App = codePush({
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-})(AppComponent);
+export const App = Sentry.wrap(
+  codePush({
+    checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  })(AppComponent),
+);
